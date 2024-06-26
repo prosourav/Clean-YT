e337232557b05c9e69f0043c278bb640d621ec41
Sourav Saha <dev.souravsaha@gmail.com>
2024-06-26 20:35:46 +0530

    Fix- merge conflicts

--- src/data/playlist-model.ts
+++ src/data/playlist-model.ts
@@ -2,6 +2,8 @@ import { action, thunk, persist, Action, Thunk } from 'easy-peasy';
 import getPlaylist from '../api';
 import { Playlist } from './types';
 import toast from 'react-hot-toast';
+import { addMinutes } from 'date-fns';
+import { cacheTime } from '../config';
 
 
 export interface PlaylistModel {
@@ -11,7 +13,7 @@ export interface PlaylistModel {
   addPlaylist: Action<PlaylistModel, Playlist>;
   setLoading: Action<PlaylistModel, boolean>;
   setError: Action<PlaylistModel, string>;
-  getPlaylist: Thunk<PlaylistModel, string>;
+  getPlaylist: Thunk<PlaylistModel, { playlistId: string; refetch?: boolean }>;
   removePlaylist: Action<PlaylistModel, string>;
 }
 
@@ -38,15 +40,27 @@ const playlistModel: PlaylistModel = persist(
     }),
 
     getPlaylist: thunk(
-      async ({ addPlaylist, setLoading, setError }, playlistId, { getState }) => {
-        if (getState().data[playlistId]) {
-          setError('Playlist already exist');
-          return toast.error('Playlist already exist');
-        }
+      async (actions, payload, helpers) => {
+        const { addPlaylist, setLoading, setError } = actions;
+        const { getState } = helpers;
+        const { playlistId, refetch = false } = payload;
+     
         if (Object.keys(getState().data).length >= 10) {
           setError('Maximum playlist size exceeded');
           return toast.error('Maximum playlist size exceeded');
         }
+
+        if (!refetch && getState().data[playlistId]) {
+          return null;
+        }
+
+        if (refetch) {
+          const lastCached = getState().data[playlistId].cache;
+          if (addMinutes(lastCached, cacheTime) > new Date()) {
+            return toast.success('PlayList is already updated');
+          }
+        }
+
         setLoading(true);
         const loadingToastId = toast.loading('loading...');
         try {
